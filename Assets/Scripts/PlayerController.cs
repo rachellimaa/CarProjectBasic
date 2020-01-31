810c61042084f707cab6baf9435ec09b655db4a4
Rachel Lima <rachel.l@samsung.com>
2020-01-31 11:35:45 -0400

    added controls foward and horizontal of car

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,20 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
     [SerializeField] private float _speed;
+
+    [SerializeField] private float _turnSpeed;
+
+    [SerializeField] private float _horizontalInput;
+
+    [SerializeField] private float _fowardInput;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * _speed);
+        _horizontalInput = Input.GetAxis("Horizontal");
+        _fowardInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * _speed * _fowardInput);
+        transform.Rotate(Vector3.up, _turnSpeed * _horizontalInput * Time.deltaTime);
     }
 }
